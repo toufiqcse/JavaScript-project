1c47f7078edc493bd1ae7c4957797acd64eb7690
toufiqcse <toufiqcse7@gmail.com>
2022-10-29 13:14:15 +0600

    convertion add

---
+++ convertion.js
@@ -0,0 +1,13 @@
+function inputcentimeter(centimeter) {
+  var meter = centimeter / 100;
+  return meter;
+}
+var getMeter = inputcentimeter(500);
+console.log(getMeter);
+// feetToInches
+function feetToInches(feet) {
+  var Inches = feet * 12;
+  return Inches;
+}
+var getInches = feetToInches(1);
+console.log(getInches);
